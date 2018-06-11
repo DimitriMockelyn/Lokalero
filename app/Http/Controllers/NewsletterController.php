abb4586d244d24f31cfe5abf64d5892463d3aae0
Oussama Keddar <oussama.keddar@epitech.eu>
2018-06-11 19:58:17 +0200

    add json responses for the subscr

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Response;
 use Illuminate\Http\Request;
 use App\ContactMail;
 use App\Http\Requests\Subscriber;
@@ -9,8 +9,12 @@ use Newsletter;
 class NewsletterController extends Controller
 {
     public function subscribe(Subscriber $request) {
-    if ( ! Newsletter::isSubscribed($request->email) ) {
-        Newsletter::subscribe($request->email);
-    }
+	    if ( ! Newsletter::isSubscribed($request->email) ) {
+	        Newsletter::subscribe($request->email);
+	        	return  ['success' => 'Vous venez de vous inscrire, merci : )'];
+	    }
+	    else {
+	    	return ['error' => 'Vous êtes déjà inscrit'];
+	    }
     }
 }
